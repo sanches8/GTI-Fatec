48ab52efda0dc4aa7dcfc98ca58414998daf2888
Sanches <soudoRock@gmail.com>
2024-05-20 20:27:03 -0300

    :books: docs:  Adição de mais um exercicio sobre POO

---
+++ 2-semestre/ILP-linguagens-programacao/POO/Exercicio-2/Produto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class Produto
+{
+    private string Nome;
+    private float Preco;
+    private int Quantidade;
+
+    // Método construtor
+    public Produto(string nome = "Lucas", float preco = 0.0f, int quantidade = 0)
+    {
+        this.Nome = nome;
+        this.Preco = preco;
+        Quantidade = quantidade;
+    }
+
+    public Produto(string nome, float preco)
+    {
+        this.Nome = nome;
+        this.Preco = preco;
+        Quantidade = 0; // Assuming default quantity is 0
+    }
+
+    public void SetNome(string nome)
+    {
+        this.Nome = nome;
+    }
+
+    public string GetNome()
+    {
+        return this.Nome;
+    }
+
+    public void SetPreco(float preco)
+    {
+        this.Preco = preco;
+    }
+
+    public float GetPreco()
+    {
+        return this.Preco;
+    }
+
+    public void SetQuantidade(int quantidade)
+    {
+        this.Quantidade = quantidade;
+    }
+
+    public int GetQuantidade()
+    {
+        return this.Quantidade;
+    }
+
+    public float CalculaValorEmEstoque()
+    {
+        return this.Quantidade * this.Preco;
+    }
+}
---
+++ 2-semestre/ILP-linguagens-programacao/POO/Exercicio-2/Program.cs
@@ -0,0 +1,24 @@
+﻿string NomeProd;
+float PrecoProd;
+int QuantProd;
+
+Console.WriteLine("Forneça o nome do primeiro produto: ");
+NomeProd = Console.ReadLine();
+Console.WriteLine("Forneça o preço do primeiro produto: ");
+PrecoProd = float.Parse(Console.ReadLine());
+Console.WriteLine("Forneça a quantidade em estoque do primeiro produto: ");
+QuantProd = int.Parse(Console.ReadLine());  
+
+Produto P1 = new Produto(NomeProd, PrecoProd, QuantProd);
+
+Console.WriteLine("Forneça o nome do segundo produto: ");
+NomeProd = Console.ReadLine();
+Console.WriteLine("Forneça o preço do segundo produto: ");
+PrecoProd = float.Parse(Console.ReadLine());
+Console.WriteLine("Forneça a quantidade em estoque do segundo produto: ");
+QuantProd = int.Parse(Console.ReadLine());
+
+Produto P2 = new Produto(NomeProd, PrecoProd, QuantProd);
+
+Console.WriteLine("O produto " + P1.GetNome() + " cujo preço é R$ " + P1.GetPreco() + " possui um estoque de " + P1.GetQuantidade() + P1.CalculaValorEmEstoque() + " em valor estocado ");
+Console.WriteLine("O produto " + P2.GetNome() + " cujo preço é R$ " + P2.GetPreco() + " possui um estoque de " + P2.GetQuantidade() + P2.CalculaValorEmEstoque() + " em valor estocado ");
