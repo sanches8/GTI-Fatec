fbf1ee01ac45da35e35f52276de57dc9c0e6fe7d
Sanches <soudoRock@gmail.com>
2024-06-03 21:13:49 -0300

    :books: docs: Adição do exercício sobre Windows Form

--- 2-semestre/ILP-linguagens-programacao/POO/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ 2-semestre/ILP-linguagens-programacao/POO/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b9b7ffaddee711a0abb7dc36099cc73b18267cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+536e6db6d8864e8260ce76b4c33bd53026fa4832")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
