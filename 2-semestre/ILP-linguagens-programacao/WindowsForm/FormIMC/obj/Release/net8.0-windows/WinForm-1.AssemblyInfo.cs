2e3c7eb56dcd314b66367e99dc9c6b124890d74c
Sanches <soudoRock@gmail.com>
2024-06-10 19:53:03 -0300

    :books: docs: Adição do conteúdo de catch

--- 2-semestre/ILP-linguagens-programacao/WindowsForm/FormIMC/obj/Release/net8.0-windows/WinForm-1.AssemblyInfo.cs
+++ 2-semestre/ILP-linguagens-programacao/WindowsForm/FormIMC/obj/Release/net8.0-windows/WinForm-1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinForm-1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbf1ee01ac45da35e35f52276de57dc9c0e6fe7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinForm-1")]
